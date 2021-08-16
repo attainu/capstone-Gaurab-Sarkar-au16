1ff441324fe0fba13eb9b6333f3a2f6a65f4b4e0
Gaurab-Sarkar-au16 <gaurab2193@gmail.com>
2021-08-16 17:46:04 +0530

    redux store, registr user, set authentication@

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import {
+  SET_CURRENT_USER,
+  SUCCESSFUL_REGISTER,
+  FAILURE_REGISTER,
+  ERRORS,
+  AUTH_ERROR,
+} from "./types";
+import { setAuthToken } from "../util/setAuthToken";
+import { getServer } from "../util/index";
+
+// set a user
+export const setCurrentUser = (user) => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+  try {
+    const res = await axios.get(`${getServer}/api/auth`);
+    dispatch({
+      type: SET_CURRENT_USER,
+      payload: res.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
+};
+
+// register a user
+export const register = (userData) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-type": "application/json",
+    },
+  };
+  try {
+    const res = await axios.post(`${getServer}/api/users`, userData, config);
+    dispatch({
+      type: SUCCESSFUL_REGISTER,
+      payload: res.data,
+    });
+  } catch (err) {
+    const error = err.response.data.errors;
+    if (error) {
+      dispatch({
+        type: ERRORS,
+        payload: error,
+      });
+    } else {
+      dispatch({
+        type: FAILURE_REGISTER,
+      });
+    }
+  }
+};
